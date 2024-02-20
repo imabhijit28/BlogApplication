60aaca0feba6c36227e228967024b69287d2d5ca
imabhijit1999 <abhijit.swain@yusata.com>
2024-02-20 11:59:01 +0530

    first commit

---
+++ src/main/java/com/blog/apis/BlogApplication/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.blog.apis.BlogApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+		System.out.println("BlogApplication Started!!!!!");
+	}
+
+}
